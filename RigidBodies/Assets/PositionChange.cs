54d2b0a85c5bf693b083dc3bec921e083c474915
Myrto <diakv115@newschool.edu>
2015-09-09 01:14:58 +0300

    Step 2

---
+++ RigidBodies/Assets/PositionChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (gameObject.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position =
+			gameObject.transform.position + new Vector3 (-.01f, .01f, 0);
+	
+	}
+}
